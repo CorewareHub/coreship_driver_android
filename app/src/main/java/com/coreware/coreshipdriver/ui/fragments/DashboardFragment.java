fc2f2429b3e9a144b9040b725f56e0321eb2724d
Jonathan Skinner <jonathan.skinner@coreware.com>
2021-02-27 10:43:38 -0700

    Added distinctive text to each view to show the navigation works correctly

--- app/src/main/java/com/coreware/coreshipdriver/ui/fragments/DashboardFragment.java
+++ app/src/main/java/com/coreware/coreshipdriver/ui/fragments/DashboardFragment.java
@@ -4,31 +4,16 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
-import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
-import androidx.fragment.app.Fragment;
-import androidx.lifecycle.Observer;
-import androidx.lifecycle.ViewModelProvider;
+
 import com.coreware.coreshipdriver.R;
-import com.coreware.coreshipdriver.ui.viewmodels.DashboardViewModel;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
 
 public class DashboardFragment extends Fragment {
 
-    private DashboardViewModel dashboardViewModel;
-
-    public View onCreateView(@NonNull LayoutInflater inflater,
-            ViewGroup container, Bundle savedInstanceState) {
-        dashboardViewModel =
-                new ViewModelProvider(this).get(DashboardViewModel.class);
+    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View root = inflater.inflate(R.layout.fragment_dashboard, container, false);
-        final TextView textView = root.findViewById(R.id.text_home);
-        dashboardViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
-            @Override
-            public void onChanged(@Nullable String s) {
-                textView.setText(s);
-            }
-        });
         return root;
     }
 }
--- app/src/main/java/com/coreware/coreshipdriver/ui/fragments/SprintListFragment.java
+++ app/src/main/java/com/coreware/coreshipdriver/ui/fragments/SprintListFragment.java
@@ -4,31 +4,17 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
-import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
-import androidx.fragment.app.Fragment;
-import androidx.lifecycle.Observer;
-import androidx.lifecycle.ViewModelProvider;
+
 import com.coreware.coreshipdriver.R;
-import com.coreware.coreshipdriver.ui.viewmodels.SprintListViewModel;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
 
 public class SprintListFragment extends Fragment {
 
-    private SprintListViewModel sprintListViewModel;
-
     public View onCreateView(@NonNull LayoutInflater inflater,
             ViewGroup container, Bundle savedInstanceState) {
-        sprintListViewModel =
-                new ViewModelProvider(this).get(SprintListViewModel.class);
         View root = inflater.inflate(R.layout.fragment_sprint_list, container, false);
-        final TextView textView = root.findViewById(R.id.text_sprint_list);
-        sprintListViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
-            @Override
-            public void onChanged(@Nullable String s) {
-                textView.setText(s);
-            }
-        });
         return root;
     }
 }
